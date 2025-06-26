4fb037d2a826d8fb39eaadba267764b2ca07d87f
Veisou <nounejm068@gmail.com>
2025-06-26 14:50:11 +0700

    Update map.js

--- map.js
+++ map.js
@@ -10,7 +10,7 @@ var map = L.map('map', {
 });
 
 let curZoom = map.getZoom();
-var bounds = [[0, 0], [6500, 6500]];
+var bounds = [[0, 0], [6500, 4500]];
 
 var image1 = L.imageOverlay('WarmWIndsMap3.png', bounds);
 var image2 = L.imageOverlay('WarmWIndsMap3.png', bounds);
